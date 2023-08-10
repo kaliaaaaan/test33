3efde3c88d2f365abcc8678deba12e5350d0f77f
kaliaaaaan <48kalian@gmail.com>
2023-08-10 16:29:13 +0300

    add todo

--- src/test/java/guru/qa/Yandextest.java
+++ src/test/java/guru/qa/Yandextest.java
@@ -1,4 +1,20 @@
 package guru.qa;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class Yandextest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy="eager";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void Yandextest() {
+       //TODO
+    }
 }
